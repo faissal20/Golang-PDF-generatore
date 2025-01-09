04465d578e1a862f731f9726d8e0e543e91a8f47
faissal20 <fayssalz15z@gmail.com>
2025-01-09 19:31:34 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module faissal20/pdfGenerator
+
+go 1.21.1
+
+require github.com/adrg/go-wkhtmltopdf v0.3.0
+
+require (
+	github.com/go-rod/rod v0.116.2 // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.40.0 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.9.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+)
+
+func main() {
+
+	bin := ""
+	// print args
+	// print all args
+
+	if len(os.Args) == 4 {
+		bin = os.Args[3]
+	} else {
+		bin = "C:/Users/dell/Downloads/chrome-win/chrome-win/chrome.exe"
+	}
+	u := launcher.New().Bin(bin).Headless(false).
+		Leakless(false).MustLaunch()
+
+	rod.New().ControlURL(u).MustConnect().MustPage(os.Args[1]).
+		MustWaitLoad().MustPDF(os.Args[2])
+
+	// clean up
+	rod.New().ControlURL(u).MustConnect().MustClose()
+
+	return
+}
